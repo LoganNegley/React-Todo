ef4824dac95808dc37ae06d8143b138dffc6b86b
Logan Negley <logannegley@gmail.com>
2020-02-12 20:43:15 -0600

    moved data back to app

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,19 +1,1 @@
-const data = [
-    {
-      task:'Cook', 
-      id: 1, 
-      completed:false
-      },
-      {
-        task:'Shop',
-        id: 2,
-        completed:false
-      },
-      {
-        task:'Homework',
-        id: 3,
-        completed:false
-      }
-      ];
 
-      export default data;
