8a67d60e54357bcc9257509f5fafd5b45adc7425
Logan Negley <logannegley@gmail.com>
2020-02-13 18:52:58 -0600

    no change

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -4,9 +4,11 @@ import React from 'react';
 
 class Todo extends React.Component {
 
+
+
   render() {
     return (
-      <div>
+      <div onClick= {()=>{}}>
           <p>{this.props.item.task}</p>
       </div>
     );
